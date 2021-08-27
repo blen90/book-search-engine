dc7d1e57f89787bb7aa39351c440ebdbe9ca9635
Blenda Orellana <blen.or90@gmail.com>
2021-08-27 15:08:04 -0500

    able to save books now

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,61 +1,67 @@
 import { gql } from '@apollo/client';
 
+//Exercise 23 - Unit 21 
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
-      token
-      user {
-        _id
-        username
-      }
+        token 
+        user {
+            _id
+            username
     }
   }
+}
 `;
 
 export const ADD_USER = gql`
-  mutation addUser($username: String!, $email: String!, $password: String!) {
+mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
-      token
-      user {
-        _id
-        username
-      }
+        token 
+        user {
+            _id
+            username
     }
   }
+}
+
 `;
 
 export const SAVE_BOOK = gql`
-  mutation saveBook($bookData: BookInput!) {
+mutation saveBook($bookData: BookInput!) {
     saveBook(bookData: $bookData) {
-      _id
-      username
-      email
-      savedBooks {
-        bookId
-        authors
-        image
-        description
-        title
-        link
-      }
+            _id
+            username
+            email
+            savedBooks {
+                bookId
+                authors
+                title
+                description
+                image
+                link
+            }
+        }
     }
-  }
+
 `;
 
 export const REMOVE_BOOK = gql`
-  mutation removeBook($bookId: ID!) {
+mutation removeBook($bookId: String!) {
     removeBook(bookId: $bookId) {
-      _id
-      username
-      email
-      savedBooks {
-        bookId
-        authors
-        image
-        description
-        title
-        link
-      }
+        user { 
+            _id
+            username
+            email
+            savedBooks {
+                bookId
+                authors
+                title
+                description
+                image
+                link
+            }
+        }
     }
-  }
+}
 `;
+
